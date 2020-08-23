b1576930f43e525f39ba19dfb1dfb0f754fc70ad
Uty KKan <Uty KKan@DESKTOP-C2MEFB8>
2020-08-23 04:09:12 -0700

    Add project files.

---
+++ week2/nozero.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int num;
+	scanf("%d", &num);
+	 if (num > 0) {
+		printf("more than zero");
+	}
+	 else {
+		printf("less or equal zero");
+	}
+	return 0;
+}
